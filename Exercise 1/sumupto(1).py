c7dfc61c29bdf2effdcb3a6839ae5b5b60204d44
Katylub <Katy@gmit.ie>
2019-02-12 15:22:53 +0000

    Second practice for problem 1

---
+++ Exercise 1/sumupto(1).py
@@ -0,0 +1,14 @@
+# Katylub, Exercise 1
+# Write a program that asks the user to input any positive integer and outputs the
+#sum of all numbers between one and that number.
+
+#Use positive number
+start=5
+ans=1
+i=1
+
+while i=start
+    ans=ans+i
+    i=i+1
+print (ans)
+
