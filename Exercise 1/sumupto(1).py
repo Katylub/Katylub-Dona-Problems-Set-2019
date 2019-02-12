681487247ddc3c019d14152dadb7d66a99bde7da
Katylub <Katy@gmit.ie>
2019-02-12 15:29:38 +0000

    Final Result Problem 1

--- Exercise 1/sumupto(1).py
+++ Exercise 1/sumupto(1).py
@@ -1,14 +1,9 @@
 # Katylub, Exercise 1
 # Write a program that asks the user to input any positive integer and outputs the
-#sum of all numbers between one and that number.
+# sum of all numbers between one and that number.
 
 #Use positive number
-start=5
-ans=1
-i=1
-
-while i=start
-    ans=ans+i
-    i=i+1
-print (ans)
+n = int(input("Input a number: "))
+sum_num = (n * (n + 1)) / 2
+print(sum_num)
 
