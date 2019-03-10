ae48c65130498026a1bbea89e11f862446114f23
Katylub <Katy@gmit.ie>
2019-03-10 15:34:05 +0000

    First draft exercise 6

---
+++ Secondstring.py
@@ -0,0 +1,9 @@
+# Katylub, exercise 6
+# Write a program that takes a user input string and outputs every second word.
+
+#User need to enter a string
+s = str(input("Input a string: "))
+
+s.split()
+
+print (s)
