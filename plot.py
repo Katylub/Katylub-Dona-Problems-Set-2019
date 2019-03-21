9101ff797802e38bb85deb510f9adf84738e3ab4
Katylub <Katy@gmit.ie>
2019-03-21 21:27:38 +0000

    Exercise 10 - Second Draft

--- plot.py
+++ plot.py
@@ -1,10 +1,18 @@
 # Katylub Dona, exercise 10
 #Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Research about Matlab in more than 10 web sites
 
-x = [0:1:100];
-y = x;
+x = np.arange (0,4)
+#first function
+y = x
+#Second function
 g = x*x
-h = 2.^x;
-plot(x, y, x, g, x, h, '.-'), legend('f(x)', 'f(x*x)', 'f(2.^x)')
+#Third function
+h = 2^x
+plt.plot(x, y, x, g, x, h, '.-')
+
+plt.show()
