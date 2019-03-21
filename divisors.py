aa611ea34e85285b5cdf9d0d0adf6e7537d4f7e8
Katylub <47187415+Katylub@users.noreply.github.com>
2019-03-21 14:39:38 +0000

    Review

--- divisors.py
+++ divisors.py
@@ -2,6 +2,8 @@
 # Write a program that prints all numbers between 1,000 and 10,000 that are divisible
 # by 6 but not 12.
 
+# I did research in the Python Tutorial and took as reference Stackoverflow (https://stackoverflow.com/questions/36760764/basic-python-for-loop)
+
 nl=[]
 for x in range(1000, 10000):
     if (x%6==0) and (x%12!=0):
